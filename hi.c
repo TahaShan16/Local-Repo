6a0a27c676ef8ff00ca96e304255a745bf17cf28
Taha Shan <tahashan09@gmail.com>
2025-04-23 09:10:33 +0500

    C

---
+++ hi.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello, World!\n");
+
+    for (int i = 0; i < 5; i++)
+    {
+        printf("This is line %d\n", i + 1);
+    }
+    return 0;
+
+
+}
